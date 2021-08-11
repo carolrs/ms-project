01975ef1d60c95e1c16723e18f5e1e137e0b6049
carolrs <anacarolinarodriguesoares@gmail.com>
2021-08-11 14:14:06 +0100

    Rest Template

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,44 @@
+package com.devsuperior.hrpayroll.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+    private long Id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker(){
+
+    }
+    public Worker(long id, String name, Double dailyIncome) {
+        Id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+}
+
