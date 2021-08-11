fbd9712fc0aecdaa11e6ebef4874ed1ae95085ca
carolrs <anacarolinarodriguesoares@gmail.com>
2021-08-11 18:28:21 +0100

    Feign Client

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,7 +2,9 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepositories;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
+    @Autowired
+    private Environment env;
     @Autowired
     private WorkerRepositories repository;
 
@@ -25,6 +29,7 @@ public class WorkerResource {
     }
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker>findById(@PathVariable Long id){
+        log.info("PORT = " + env.getProperty("local.server.port"));
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
