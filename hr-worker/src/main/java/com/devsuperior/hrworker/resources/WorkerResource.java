ccc4b1a7fb01784876651457019df584bad0b728
carolrs <anacarolinarodriguesoares@gmail.com>
2021-08-17 16:42:10 +0100

    Hystrix fault tolerance

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -2,7 +2,8 @@ package com.devsuperior.hrworker.resources;
 
 import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepositories;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@Slf4j
+
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+
+    private static Logger logger= LoggerFactory.getLogger(WorkerResource.class);
 
     @Autowired
     private Environment env;
@@ -29,7 +32,15 @@ public class WorkerResource {
     }
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker>findById(@PathVariable Long id){
-        log.info("PORT = " + env.getProperty("local.server.port"));
+
+        /*
+        try {
+            Thread.sleep(3000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
