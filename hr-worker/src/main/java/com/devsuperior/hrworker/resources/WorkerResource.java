dd6f1c3bfdd2331db31d2d9b6a7249203db9ff60
carolrs <anacarolinarodriguesoares@gmail.com>
2021-08-23 16:30:40 +0100

    Configuring Zuul api gateway Config server hr-payroll hr-worker

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import com.devsuperior.hrworker.repositories.WorkerRepositories;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,16 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
     private static Logger logger= LoggerFactory.getLogger(WorkerResource.class);
 
+   @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment env;
     @Autowired
     private WorkerRepositories repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void>getConfigs(){
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>>findAll(){
@@ -33,12 +45,11 @@ public class WorkerResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker>findById(@PathVariable Long id){
 
-        /*
         try {
             Thread.sleep(3000L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
         Worker obj = repository.findById(id).get();
