085a3d0cbf9f364f5b97d8334db65f2db094a763
carolrs <anacarolinarodriguesoares@gmail.com>
2021-08-25 15:30:57 +0100

    Project hr-oauth created.

---
+++ hr-oauth/src/test/java/devsuperior/com/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package devsuperior.com.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
